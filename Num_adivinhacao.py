60f43be4d1c60f0e3e06c03f6a13046c59d3c600
FLAVIANE <flaviane.costa.dias@hotmail.com>
2023-02-22 21:07:11 -0300

    Adivinhe o numero maiorOUmenor v2

--- Num_adivinhacao.py
+++ Num_adivinhacao.py
@@ -1,4 +1,6 @@
 #utilizei a mesma forma que aprendi na video aula porem eu faria diferente. Definiria de cara a variavel num
+# (sep="_") coloca o separador obrigatorio
+#nova att informar se chute foi alto ou baixo
 
 print("*********************************")
 print("Bem vindo ao jogo de Adivinhação!")
@@ -6,13 +8,19 @@ print("*********************************")
 
 numero_secreto = 42
 
-chute_str = input("Digite o seu número: ")
-print("Você digitou: ", chute_str)
+chute_str = input("\n Digite o seu número: ")
+print("\n Você digitou: ", chute_str)
 chute = int(chute_str)
 
 if (numero_secreto == chute):
-    print("Você acertou!")
-else:
-    print("Você errou!")
+    print("\n Você acertou!")
 
+else:
+    if (chute > numero_secreto):
+        print("\n Você errou! O seu chute foi maior que o número secreto.")
+    elif (chute < numero_secreto):
+        print('\n Você errou! O seu chute foi menor que o numero secreto.')
+
+print("\n*********************************")
 print("Fim do jogo")
+print("*********************************")
